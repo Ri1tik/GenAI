4c6784228e52e765c404e4ba2c1f26cbe0a44100
Ritikydv1 <ritik.r.yadav0001@gmail.com>
2026-03-11 19:38:35 +0530

    Initial commit: Setup with backend and DB connection

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require("./src/app");
+require("dotenv").config();
+const dbConnection = require("./src/config/database");
+
+const PORT = process.env.PORT || 4000;
+
+dbConnection()
+    .then(() => {
+        console.log("Database connected successfully");
+        app.listen(PORT, () => {
+            console.log('Server is running on port 3000');
+        })
+    }).catch((err) => {
+        console.log("Database connection failed", err);
+    });
